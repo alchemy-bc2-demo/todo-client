079cd88ab283015e70fb1a621db8a59d0b7b9a16
martypdx <marty@martypdx.com>
2018-03-11 18:10:14 -0700

    initial setup

---
+++ scripts/models/todo.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const API_URL = 'http://localhost:3000';
+
+(function (module) {
+
+    const template = Handlebars.compile($('#todo-template').html());
+    
+    function Todo(data) {
+        Object.keys(data).forEach(key => this[key] = data[key]);
+    }
+    
+    Todo.prototype.toHtml = function() {
+        return template(this);
+    };
+
+    // Define "instance" data methods
+    Todo.prototype.insert = function(callback) {
+        $.post(`${API_URL}/todos`, {
+            task: this.task
+        })
+            .then(data => {
+                Object.keys(data).forEach(key => this[key] = data[key]);
+                Todo.all.push(this);
+                if(callback) callback();
+            });
+    };
+    
+    Todo.all = [];
+    
+    Todo.fetchAll = function(callback) {
+        $.getJSON(`${API_URL}/todos`)
+            .then(data => {
+                Todo.all = data.map(each => new Todo(each));
+                if(callback) callback();
+            })
+            .catch(console.log);
+    };
+
+    module.Todo = Todo;
+
+})(window.app || (window.app = {}));
